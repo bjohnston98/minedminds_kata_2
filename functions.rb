934aab523c478f8b452e33b522e99e59166e3fa6
Brian Johnston <b_johnston98@yahoo.com>
2017-01-19 09:28:20 -0500

    both work, one calls the other

---
+++ functions.rb
@@ -0,0 +1,3 @@
+def mined_minds_function(number)
+	1 #just to get it to pass NOW
+end
--- testfunctions.rb
+++ testfunctions.rb
@@ -1,7 +1,14 @@
-require 'minitest/autorun' #part of ruby library
+require 'minitest/autorun' #part of ruby library and a ruby gem
+require_relative 'functions.rb' #we have to use require_relative becuse of 
 
-class TestMMFunctions < Minitest::Test #Testblah can be any name, Class part of lib
+class TestMMFunctions < Minitest::Test 
 	def test_one_equals_one() #start the function
 		assert_equal(1,1) #watchfortypos, OW actually said "did you mean equal, not equals"
 	end
+
+	def test_pass_1_to_MMfunction_gives_1
+		assert_equal(1, mined_minds_function(1))
+	end
+
 end	#this seems to work
+
